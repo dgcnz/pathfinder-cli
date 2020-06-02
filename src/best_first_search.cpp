9c13ce4ae7eb3911cf66fc18a7b46a51f316a3aa
Diego Cánez <canezdiego@gmail.com>
2020-06-02 15:53:44 -0300

    best first

--- src/best_first_search.cpp
+++ src/best_first_search.cpp
@@ -1,10 +1,50 @@
-#include "best_first_search.hpp"
 #include "types.hpp"
+#include "utils.hpp"
+
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
 
 path best_first_search(maze m, opt_payload o)
 {
-    // TODO
-    (void)(o); // remove if variable is used
-    (void)(m); // remove if variable is used
-    return path();
+    point target = m.target;
+
+    map<point, point> came_from;
+    map<point, bool>  visited;
+
+    auto dist         = o.dist;
+    auto h            = [dist, target](point p) { return dist(p, target); };
+    auto f            = [h](point p) { return h(p); };
+    auto increasing_f = [f](point p1, point p2) -> bool {
+        return f(p1) < f(p2);
+    };
+    multiset<point, decltype(increasing_f)> frontier(increasing_f);
+
+    frontier.insert(m.start);
+
+    while (not frontier.empty())
+    {
+        point current    = *frontier.begin();
+        visited[current] = true;
+
+        if (current == target)
+            return reconstruct_path(came_from, current);
+
+        frontier.erase(frontier.begin());
+
+        for (auto q : neighbors(current, m))
+        {
+            if (visited.find(q) == visited.end()) // not visited
+            {
+                came_from[q] = current;
+                frontier.insert(q);
+            }
+        }
+    }
+    cout << "NO PATH WAS FOUND" << endl;
+    return {}; // No path was found
 }
